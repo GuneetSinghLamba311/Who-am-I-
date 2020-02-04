837a76e3ebf47470acce72ea3031631f6b973a64
Guneet Singh Lamba <lambaguneetsingh@gmail.com>
2020-02-03 22:18:26 -0500

    Initial Commit

---
+++ who am I ?/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  who am I ?
+//
+//  Created by GuneetSingh Lamba on 2020-02-03.
+//  Copyright © 2020 GuneetSingh Lamba. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
